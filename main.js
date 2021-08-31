d1ddbae95973efc6ebc1164f7596684734a5bade
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-08-31 14:31:19 -0600

    Archivos para comenzar mi proyecto

---
+++ main.js
@@ -0,0 +1,23 @@
+fetch("https://weatherapi-com.p.rapidapi.com/current.json?q=80140%20culiacan", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+		"x-rapidapi-key": "0c5911963bmsh430fa4f73d71a5dp14dc20jsn3edb351cb442"
+	}
+})
+.then(response => {
+    console.log('hola');
+    response.json().then(function(data) {
+        EXTRACTOR(data);
+    });
+})
+.catch(err => {
+	console.error(err);
+});
+
+
+
+
+function EXTRACTOR(datos) {
+    console.log(datos);
+}
